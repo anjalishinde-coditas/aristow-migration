f67eb6c1aba34b5624bc5df0180c7ee42f2b5b8c
Navin Gautam <navin.gautam@coditas.com>
2025-06-24 16:34:19 +0530

    sso login initial changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/component/header/header.component';
+import { MsalService } from '@azure/msal-angular';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,16 @@ import { HeaderComponent } from './shared/component/header/header.component';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'aristow-migration';
+  constructor(private msalService: MsalService) {}
+
+  ngOnInit(): void {
+    this.msalService.instance.handleRedirectPromise().then((res) => {
+      const account = res?.account ?? this.msalService.instance.getAllAccounts()[0];
+      if (account) {
+        this.msalService.instance.setActiveAccount(account);
+      } else {
+        this.msalService.loginRedirect(); // 👈 Auto login if not already logged in
+      }
+    });
+  }
 }
